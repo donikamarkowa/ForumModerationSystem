11912dbd293c197f14ddab0e58c22443eab9212a
donikamarkowa <donimarkova@gmail.com>
2025-06-13 14:42:11 +0300

    Add SentimentAnalysisService for predicting comment sentiment using ML.NET model

---
+++ AutoModeratedForum/AutoModeratedForum/Services/SentimentAnalysisService.cs
@@ -0,0 +1,26 @@
+﻿using AutoModeratedForum.MLModels;
+using Microsoft.ML;
+using System.IO;
+
+namespace AutoModeratedForum.Services
+{
+    public class SentimentAnalysisService
+    {
+        private readonly PredictionEngine<CommentInput, CommentPrediction> _predictionEngine;
+
+        public SentimentAnalysisService(MLContext mlContext, string modelPath)
+        {
+            if (!File.Exists(modelPath))
+                throw new FileNotFoundException("ML model not found at: " + modelPath);
+
+            ITransformer mlModel = mlContext.Model.Load(modelPath, out var modelInputSchema);
+            _predictionEngine = mlContext.Model.CreatePredictionEngine<CommentInput, CommentPrediction>(mlModel);
+        }
+
+        public CommentPrediction Predict(string commentText)
+        {
+            var input = new CommentInput { Text = commentText };
+            return _predictionEngine.Predict(input);
+        }
+    }
+}
