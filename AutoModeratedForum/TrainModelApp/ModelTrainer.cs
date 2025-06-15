c85f6874f9881d94320a1bce3ef0d9f472796f9e
donikamarkowa <donimarkova@gmail.com>
2025-06-15 19:30:12 +0300

    Add new propertu PredictedLabel and remove isRude

--- AutoModeratedForum/TrainModelApp/ModelTrainer.cs
+++ AutoModeratedForum/TrainModelApp/ModelTrainer.cs
@@ -2,14 +2,13 @@
 
 public class CommentInput
 {
-    public string Text { get; set; }
+    public string Text { get; set; } = null!;
     public bool Label { get; set; }
 }
 
 public class CommentPrediction
 {
-    public bool isRude { get; set; }
-
+    public bool PredictedLabel { get; set; }
     public float Probability { get; set; }
     public float Score { get; set; }
 }
